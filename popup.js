c9c5c5ea48b85df8730d6c4720a5bf770803e497
morris <nike4859@gmail.com>
2017-04-03 11:19:18 +0800

    Typo

--- popup.js
+++ popup.js
@@ -291,7 +291,7 @@ function downloadePub() {
         downloadErrorFlag = false; //download error stop
         dir = sanitize(bookName, '');
         index = 0;
-        ∂downloadLinks(links, dir, index, mode);
+        downloadLinks(links, dir, index, mode);
     }
 
     //http://voler.ebookservice.tw/epub/fetch/bookId/token/bookToken/OEBPS/content.opf
